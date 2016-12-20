f63b3c5841568d378c2d72daf4a065f5f1ec9d74
DESKTOP-RRSNALA\John <johannes.schnee@honacon.com>
2016-12-20 23:25:59 +0100

    Add project files.

---
+++ hangm/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace hangm
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+
+        private int wrongGuesses = 0;
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            Application.Current.DebugSettings.EnableFrameRateCounter = false;
+        }
+
+        public static object Properties { get; private set; }
+
+        private void guessClick(object sender, RoutedEventArgs e)
+        {
+            wrongGuesses++;
+            if (wrongGuesses > 7)
+            {
+                
+            }
+
+        }
+    }
+}
+
+
+
